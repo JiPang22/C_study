2539ab0eb18fc22c3a9eeea4176629810933c24d
JiPang22 <wlvud6902@gmail.com>
2025-04-11 21:37:16 +0900

    greet 및 fun2 함수 추가, 프로젝트 설정 파일 생성
    FileName.c, FileName2.c, FileName3.c에서 greet()와 fun2() 함수 정의 및 호출 추가.
    FileName2.c에서 age 변수를 정의하고 greet()에서 조건부 출력 구현.
    FileName3.c에서 BMP 파일 헤더 및 정보 헤더 정의, fun2()에서 BMP 파일 읽기 로직 추가.
    pic.png 파일 삭제.
    CppProperties.json, Project7.sln, Project7.vcxproj, Project7.vcxproj.filters 파일 추가하여 Visual Studio 설정 및 프로젝트 구성 포함.

--- FileName2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int age = 10;
-void greet() {
-  if (age == 10) {
-    printf("Hello from greet.c!\n");
-
-  }
-  
-}
---
+++ Project7/Project7/FileName.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// greet 함수의 선언 (프로토타입)
+void greet();
+int fun2();
+
+int main() {
+  greet();  // 보조 함수 호출
+  fun2();
+  return 0;
+}
